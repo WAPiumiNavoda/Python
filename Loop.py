1b6267c775ebcbeb39d89d08ddf8fd08a7a85cad
Piumi Navoda <navodapiumi691@gmail.com>
2023-07-25 11:16:54 +0530

    while loop

--- Loop.py
+++ Loop.py
@@ -1,3 +1,27 @@
 x = [ 10,20,30,40 ]
+index = 0
+# for item in x:
+#     y = x[index]
+#     print(index,y)
+#     index += 1
 
-for item in x:
+
+# for item in enumerate(x):
+#     index = item[0]
+#     value = item[1]
+#     print(index,value)
+#     # print(type(item), item)
+
+# for index,value in enumerate(x):
+#     print(index,value)
+#     # print(type(item), item)
+
+r =range(0, 10)
+print(type(r))
+for item in range(0, 10):
+    print(item)
+   
+r =range(0, len(x))
+
+for item in r:
+    print(x[item])
---
+++ WhileLoop.py
@@ -0,0 +1,7 @@
+x = [10,20,30,40,50]
+
+count = 0
+
+while count < 10:
+    print("Count " , count)
+    count += 1
