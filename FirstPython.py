00a8c0915fbde009a15a0ac6aef0840c36e16b0e
Piumi Navoda <navodapiumi691@gmail.com>
2023-07-25 09:32:05 +0530

    python loop

---
+++ FirstPython.py
@@ -0,0 +1,96 @@
+# print("Hello World")
+# print('Hello World')
+# print(2.156)
+# print(123)
+# print(False)
+# print("Hello \n Piumi")
+# print("Hello \n Piumi")
+# print("Name\t: Piumi")
+# print("Age\t: 23")
+# print("Saman said \"Good Morning\"")
+# print("I need a \\n new line....")
+
+# name = "Piumi"
+# print(name)
+
+# a = 2
+# b = 3
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a % b)
+# print(a // b)
+# print(a ** b)
+
+# a = "Hello "
+# b = "Piumi"
+# c = 5
+# d = a + b + " " + str(c)
+# print(type(d))
+
+# print("Enter your name: " , end='')
+# name = input()
+# print("My name is " + name)
+
+# x = [122,133,144,155]
+# y = [10,20,30,40,50]
+
+# print(x + y)
+# is_10_in_z = 10 in y
+# print(is_10_in_z)
+
+
+# x = {'1000':"colombo" , "2000":"panadur"}
+# x['3000'] = "Gampaha"
+# x['4000'] = {"Kaduvela","Meerigama"}
+
+# print(x)
+# print(x.keys())
+# print(x.values())
+# # del x[0]
+# del x["4000"]
+# print(x)
+
+# x = {1: 'a' , 2: 'b'}
+# y = x.get(2,0)
+# print(y)
+
+# x = {
+#     "a":['Hello' ,"Good morning" ,"Hi"],
+#     "b":["Bye","Take care"]
+# }
+# y = x['a']
+# y.append("heyy")
+# print(y)
+
+# x = {"A","B","B","C"}
+# x.add("a")
+# print(x)
+
+# Praneeth = ("praneeth", 176 ,"Sri Lanka","Germany")
+# x  = ("1" , 2 ,"Rawee");
+# print(type(Praneeth))
+
+# name  = Praneeth[2]
+# print(name)
+
+# index , name , letter = x
+# print(name)
+
+# //Slicing
+
+# x = ['a' , 'b' ,'c', 'd']
+# y = x[2:6]
+# print(y)
+# z = x[-1]
+# print(z)
+# print(len(y))
+
+marks = 75
+
+if marks<=50 and marks>75:
+    print("A")
+elif marks >=35 and marks <55:
+    print("")
